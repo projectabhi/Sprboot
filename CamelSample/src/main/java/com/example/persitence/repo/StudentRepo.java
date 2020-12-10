2e6c3a2de864c5abdb5ad1bf4aeb695a728abb60
projectabhi <projectabhi@gmail.com>
2020-12-10 12:44:14 +0530

    Added hikari datasource changes

--- CamelSample/src/main/java/com/example/persitence/repo/StudentRepo.java
+++ CamelSample/src/main/java/com/example/persitence/repo/StudentRepo.java
@@ -3,7 +3,9 @@ package com.example.persitence.repo;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.persitence.model.Student;
+
 @Repository
-public interface StudentRepo extends CrudRepository<StudentRepo, Long> {
+public interface StudentRepo extends CrudRepository<Student, Long> {
 
 }
--- CamelSample/src/main/java/com/example/processor/StudentProcessor.java
+++ CamelSample/src/main/java/com/example/processor/StudentProcessor.java
@@ -1,17 +1,26 @@
 package com.example.processor;
 
+import java.util.List;
+import java.util.Random;
+
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.borokali.model.res.StudentResponse;
+import com.example.persitence.model.Student;
+import com.example.persitence.repo.StudentRepo;
 
 @Component
 public class StudentProcessor implements Processor{
 	private static final Logger log=LogManager.getLogger(StudentProcessor.class);
 
+	@Autowired
+	private StudentRepo studentRepo;
+	
 	@Override
 	public void process(Exchange exchange) throws Exception {
 		log.info(exchange.getIn().getBody());
@@ -26,6 +35,11 @@ public class StudentProcessor implements Processor{
 		exchange.setProperty("processor1", new Integer(1));
 		StudentResponse response=new StudentResponse();
 		
+		Student student=new Student();
+		student.setFirstName("Test");
+		student.setLastName("Test");
+		Student st=studentRepo.save(student);
+		log.info("Saved Student:"+st.getId());
 		exchange.setProperty("response",response);
 		
 	}
