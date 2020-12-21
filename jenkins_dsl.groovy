6b0143dd563e4726a190d7ea54a82d73cda2a457
projectabhi <projectabhi@gmail.com>
2020-12-21 18:58:57 +0530

    Update jenkins_dsl.groovy

--- jenkins_dsl.groovy
+++ jenkins_dsl.groovy
@@ -1,27 +1,30 @@
 def project = 'Sprboot'
 def branchApi = new URL("https://api.github.com/repos/projectabhi/${project}/branches")
 def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
+
+folder('MyFolder') {
+	displayName('MyFolder')
+	description('MyFolder')
+}
+
 branches.each {
-	folder('MyFolder') {
-		displayName('MyFolder')
-		def branchName = it.name
-		def jobName = "${project}-${branchName}".replaceAll('/','-')
-		pipelineJob(jobName) {
-			def repo = "https://github.com/projectabhi/${project}.git"
-			definition {
-		    		cpsScm {
-		      			scm {
-						git {
-			  				remote { 
-								url(repo) 
-							}
-			  			branch("${branchName}")
-			  			scriptPath('CamelSample/Jenkinsfile')
-			  			extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+	def branchName = it.name
+	def jobName = "${project}-${branchName}".replaceAll('/','-')
+	pipelineJob(jobName) {
+		def repo = "https://github.com/projectabhi/${project}.git"
+		definition {
+			cpsScm {
+				scm {
+					git {
+						remote { 
+							url(repo) 
 						}
-		      			}
-		    		}
-		  	}
+					branch("${branchName}")
+					scriptPath('CamelSample/Jenkinsfile')
+					extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+					}
+				}
+			}
 		}
 	}
 }
